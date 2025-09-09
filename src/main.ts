f84546cf614c24c1b250bc8042e394965965b921
Andres Vallecilla <andresfelipe083195@hotmail.com>
2025-09-08 22:14:42 -0500

    chrore: add config file
    Signed-off-by: Andres Vallecilla <andresfelipe083195@hotmail.com>

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { ValidationPipe, VersioningType } from '@nestjs/common';
+import { PORT } from './config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -24,10 +25,9 @@ async function bootstrap() {
   // Pipes
   app.useGlobalPipes(new ValidationPipe());
 
-  const port = process.env.PORT ?? 3000;
-  await app.listen(port);
+  await app.listen(PORT);
 
-  console.log(`Server running on http://localhost:${port}`);
+  console.log(`Server running on http://localhost:${PORT}`);
 
 }
 bootstrap();
